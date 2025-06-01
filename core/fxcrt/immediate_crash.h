1919d1142d9b56b2bf3515565ad4906a185b7a2c
Christian O. Venegas <cvenegas@esri.com>
2025-05-31 20:03:37 -0700

    Fix volatile not in msvc
    https://stackoverflow.com/questions/53345462/compiler-complaining-about-volatile-keyword

--- core/fxcrt/immediate_crash.h
+++ core/fxcrt/immediate_crash.h
@@ -91,22 +91,8 @@
 
 #elif defined(COMPILER_MSVC)
 
-#if defined(ARCH_CPU_ARM64)
-
-// Windows ARM64 uses "BRK #F000" as its breakpoint instruction, and
-// __debugbreak() generates that in both VC++ and clang.
 #define TRAP_SEQUENCE1_() __debugbreak()
-// Intentionally empty: __builtin_unreachable() is always part of the sequence
-// (see IMMEDIATE_CRASH below) and already emits a ud2 on Win64,
-// https://crbug.com/958373
-#define TRAP_SEQUENCE2_() __asm volatile("")
-
-#else
-
-#define TRAP_SEQUENCE1_() asm volatile("int3")
-#define TRAP_SEQUENCE2_() asm volatile("ud2")
-
-#endif  // defined(ARCH_CPU_ARM64)
+#define TRAP_SEQUENCE2_()
 
 #else
 
